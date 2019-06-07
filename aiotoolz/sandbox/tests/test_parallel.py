d69d775491f5af80bbf37773d359c41628c6ee10
Ethan Brouwer <eabrouwer3@gmail.com>
2019-06-07 14:01:01 -0600

    Forgot to add the aiotlz and aiotoolz when committing.

---
+++ aiotoolz/sandbox/tests/test_parallel.py
@@ -0,0 +1,24 @@
+from aiotoolz.sandbox.parallel import fold
+from aiotoolz import reduce
+from operator import add
+from pickle import dumps, loads
+
+# is comparison will fail between this and no_default
+no_default2 = loads(dumps('__no__default__'))
+
+
+def test_fold():
+    assert fold(add, range(10), 0) == reduce(add, range(10), 0)
+    assert fold(add, range(10), 0, chunksize=2) == reduce(add, range(10), 0)
+    assert fold(add, range(10)) == fold(add, range(10), 0)
+
+    def setadd(s, item):
+        s = s.copy()
+        s.add(item)
+        return s
+
+    assert fold(setadd, [1, 2, 3], set()) == {1, 2, 3}
+    assert (fold(setadd, [1, 2, 3], set(), chunksize=2, combine=set.union)
+            == {1, 2, 3})
+
+    assert fold(add, range(10), default=no_default2) == fold(add, range(10))
